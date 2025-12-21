d936b6f8386587e6dc594d0dae74b280a9ef5dc8
Ezaj24 <workazaj@gmail.com>
2025-12-21 13:48:40 +0530

    Fix Post Endpoint with Dto mapping and CreatedAtAction

--- WorkItemManagement.Api/Controllers/WorkItemsController.cs
+++ WorkItemManagement.Api/Controllers/WorkItemsController.cs
@@ -21,26 +21,43 @@ namespace WorkItemManagement.Api.Controllers
         public async Task<IActionResult> GetAll()
         {
             var items = await _service.GetAllAsync();
-            return Ok(items);
+
+            var response = items.Select(x => new WorkItemResponseDto
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description,
+                IsCompleted = x.IsCompleted,
+            });
+
+            return Ok(response);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
             var item = await _service.GetByIdAsync(id);
-            if (item == null)
+            if(item  == null)
+            {
                 return NotFound();
+            }
 
-            return Ok(item);
+            var resposne = new WorkItemResponseDto
+            {
+                Id = item.Id,
+                Title = item.Title,
+                Description = item.Description,
+                IsCompleted = item.IsCompleted,
+            };
+
+            return Ok(resposne);
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateWorkItemDto dto)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
 
             var item = new WorkItem
             {
@@ -51,8 +68,22 @@ namespace WorkItemManagement.Api.Controllers
 
             var created = await _service.CreateAsync(item);
 
-            return CreatedAtAction(nameof(GetById), new {id = created.Id},created);
+            var response = new WorkItemResponseDto
+            {
+                Id = created.Id,
+                Title = created.Title,
+                Description = created.Description,
+                IsCompleted = created.IsCompleted
+            };
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = response.Id },
+                response
+            );
         }
+
+
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, [FromBody] UpdateWorkItemDto dto)
